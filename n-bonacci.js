3f933e7b290e35aac1146bfd44822b62c42757fb
Juan Pablo Chavez Arteaga <juanpablo.chavez@training.assuresoft.com>
2023-12-22 07:03:08 -0400

    Nbonacci bottom up done

--- n-bonacci.js
+++ n-bonacci.js
@@ -1,22 +1,20 @@
+function nBonacci(n, m) {
+    let cache = Array(m).fill(0);
+    if (n <= m) {
+        cache[n - 1] = 1;
+    }
+    // for (let i = n; i < m; i++) {
+    //     cache[i] = cache.slice(i - n, i).reduce((sum, val) => sum + val, 0);
+    // }
+    Array(m - n).fill().forEach((_, index) => {
+        let actualIndex = index + n;
+        // console.log(cache);
+        // console.log(cache.slice(actualIndex - n, actualIndex));
+        cache[actualIndex] = cache.slice(actualIndex - n, actualIndex).reduce((sum, val) => sum + val, 0);
+    });
 
-let cache = [];
-const nBonacci = (n, m) => {
-    cache = [...Array(m)].map((elem, i) => i < n - 1 ? 0 : false);
-    cache[n - 1] = 1;
-    console.log(cache, 'initial');
-    return nBonacciHelper(n, m);
-};
-let count = 0;
-const nBonacciHelper = (n, m) => {
-    console.log('call:',count++, n, m);
-    if (m < n - 1) return 0;
-    if (cache[m - 1]) return cache[m - 1];
-
-    console.log(cache, 'before');
-
-    cache[m - 1] = cache.reduce((sum, elem, i) => { return sum + (i > (m - n - 1)) ? nBonacciHelper(n, i) : 0 }, 0);
-    console.log(cache, 'after');
     return cache[m - 1];
 }
 
-console.log(nBonacci(3, 6));
+console.log(nBonacci(2, 6));
+console.log(nBonacci(3, 7));
